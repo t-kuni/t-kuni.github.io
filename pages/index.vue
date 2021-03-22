62cdebb9c6535459144b2807fa6a89dc90f8eae0
t-kuni <t.kuni.dev@gmail.com>
2021-03-22 19:35:25 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,69 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        profile-2
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <section>
+      <div class="flex justify-center">
+        <img src="~/assets/img/avatar.jpg" class="rounded-full">
       </div>
-    </div>
+      <h1 class="font-bold text-lg text-accent text-center">T・Kuni</h1>
+      <p class="text-center">Webエンジニア　1990年生まれ</p>
+      <p class="m-3">アプリケーションレイヤが主戦場のWebエンジニアです。<br/>
+        エンドユーザ目線でUXの改善、機能の提案＆実装を心掛けています。<br/>
+        プログラミングやDB設計、SEO、インフラ構築、UX/UIデザインなど幅広く対応しています。<br/>
+        目的や課題から逆算して必要なものを習得、実装するスタイルなので広く浅くフルスタック気味です。<br/>
+        特定の技術に凄く詳しいというよりは、必要なスキルを広く浅く習得しています。</p>
+    </section>
+    <section>
+      <h2 class="line-header">資格</h2>
+      <ul>
+        <li>基本情報処理</li>
+        <li>応用情報処理</li>
+      </ul>
+    </section>
+    <section>
+      <h2 class="line-header">リンク</h2>
+      <ul>
+        <li>aaa</li>
+      </ul>
+    </section>
+    <section class="flex overflow-hidden">
+      <div class="section-side-bar flex-none">
+
+      </div>
+      <div class="flex-auto">
+        <h2 class="text-accent">仕事との向き合い方</h2>
+        <p>趣味である個人制作を通して習得した知識を仕事で活用する事が多いです。
+          個人制作では試したい事や使用する技術を決めて、ガッと開発に取り組んで、ブログなりQiitaなりにアウトプットする事で知識が身につくようにしています。
+          プライベートの時間を使った自己研鑽もある程度評価して頂ける風土があると嬉しいです。</p>
+        <p>必要に応じてユニットテストを書きます。
+          全てのモジュール、パターンに対して書くとコストが跳ね上がってしまうので、ビジネス的に重要なモジュールや、複雑になる事が予想されるモジュールに対して書くようにしています。</p>
+      </div>
+    </section>
+    <section>
+      <h2>経歴</h2>
+      <h3>自社サービス企業</h3>
+      <p>2017 〜 現在</p>
+      <p>経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文</p>
+      <h3>SES（技術者派遣）企業</h3>
+      <p>2012 〜 2017</p>
+      <p>経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文</p>
+      <h3>情報系専門</h3>
+      <p>2010 〜 2012</p>
+      <p>経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文</p>
+      <h3>情報系専門</h3>
+      <p>2010 〜 2012</p>
+      <p>経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文経歴本文</p>
+    </section>
+    <section>
+      <h2>使い慣れている技術スタック</h2>
+      <table>
+        <tbody>
+        <tr>
+          <th>言語</th>
+          <td>PHP, JavaScript(ブラウザ、Node), SQL</td>
+        </tr>
+        </tbody>
+      </table>
+    </section>
   </div>
 </template>
 
@@ -32,47 +72,4 @@ export default {}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      accent: {
+        DEFAULT: '#53B6B6',
+        light: '#84DADA',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
